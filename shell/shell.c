d4526ad84b42619ce059eacf975eb5464e6af786
Jervis Muindi <jervis@ubuntu.(none)>
2012-09-09 17:30:48 -0400

    add shell header file

--- shell/shell.c
+++ shell/shell.c
@@ -1,4 +1,15 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "shell.h"
+
 int main(int argc, char **argv)
 {
+	test_print();
+	return 0;
+}
+
+int test_print(void) {
+	printf(TEXT);
 	return 0;
 }
---
+++ shell/shell.h
@@ -0,0 +1,13 @@
+/*
+ * shell.h
+ *
+ *  Created on: Sep 9, 2012
+ */
+
+#ifndef SHELL_H_
+#define SHELL_H_
+
+#define TEXT "test text"
+int test_print(void);
+
+#endif /* SHELL_H_ */
